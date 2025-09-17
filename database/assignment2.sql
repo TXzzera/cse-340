9a204703b0d2fb6adaab803a4aa3d6d626a4bf9a
TXzzera <brunoteixeira.10@hotmail.com>
2025-09-16 22:50:18 -0300

    att

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -25,8 +25,8 @@ WHERE classification.classification_name = 'Sport';
 UPDATE
   public.inventory
 SET 
-  inv_image = REPLACE (inv_image,'/images/vehicles/vehicles', '/images/vehicles/'),
-  inv_thumbnail = REPLACE (inv_thumbnail,'/images/vehicles/vehicles', '/images/vehicles/');
+  inv_image = REPLACE (inv_image,'/images/', '/images/vehicles/'),
+  inv_thumbnail = REPLACE (inv_thumbnail,'/images/', '/images/vehicles/');
 
 
 
