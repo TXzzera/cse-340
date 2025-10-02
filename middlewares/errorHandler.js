928919b0e9bc8d4941697ebb403654eee7aa004b
TXzzera <brunoteixeira.10@hotmail.com>
2025-10-01 22:27:51 -0300

    att final

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,21 @@
-function errorHandler(err, req, res, next) {
+const Util = require('../utilities');
+async function errorHandler(err, req, res, next) {
   console.error(err.stack);
-  res.status(500).render('error', { message: err.message });
+
+ 
+  let navHTML;
+  try {
+    navHTML = await Util.getNav(req, res, next);
+  } catch (navErr) {
+    console.error("Error:", navErr);
+    navHTML = '<ul><li><a href="/">Home</a></li></ul>'; 
+  }
+
+  res.status(500).render('error', { 
+    message: err.message,
+    title: '500 Error',
+    nav: navHTML
+  });
 }
 
 module.exports = errorHandler;
